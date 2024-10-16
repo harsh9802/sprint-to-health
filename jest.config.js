d5bacbbf922f9413eb03036f5cea01d7e5773696
sharvkk <sharv.sharv@gmail.com>
2024-10-15 22:48:59 -0400

    Added jest configs

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+export default {
+    transform: {
+      "^.+\\.js$": "babel-jest",  // Use Babel to transform JavaScript files
+    },
+    testEnvironment: "node", // Optional, ensures that Node.js environment is used
+  };
+  
