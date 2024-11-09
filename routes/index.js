0e28c7843464c407fd93b5aa19846798f9791541
Vijay Khot <vijaysinh.khot@gmail.com>
2024-11-08 19:56:06 -0500

    Refactored routes and middlewares

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import userRouter from "./userRoutes.js";
+import llmRouter from "./llmRoutes.js";
+import dashboardRouter from "./dashboardRoutes.js";
+import vitalsRouter from "./vitalsRoutes.js";
+import interactionsRouter from "./interactionsRoutes.js";
+
+export {
+  userRouter,
+  llmRouter,
+  dashboardRouter,
+  vitalsRouter,
+  interactionsRouter,
+};
---
+++ utils/middlewares/index.js
@@ -0,0 +1,11 @@
+import compressionMiddleware from "./compressionMiddleware.js";
+import loggingMiddleware from "./loggingMiddleware.js";
+import rateLimitMiddleware from "./rateLimitMiddleware.js";
+import securityMiddleware from "./securityMiddleware.js";
+
+export {
+  compressionMiddleware,
+  loggingMiddleware,
+  rateLimitMiddleware,
+  securityMiddleware,
+};
